4f7770894ea5370cd33c775d55bbca8ba7c46d78
JongCo <xn1128@gmail.com>
2023-01-11 00:52:37 +0900

    dotenv관련 사항 추가, mongodb 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+console.log(process.env.NODE_ENV);
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.', process.env.NODE_ENV].join("")
+    }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
