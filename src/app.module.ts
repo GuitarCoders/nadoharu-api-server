32d035473dd93b2272048122415c72cf05e2569e
JongCo <xn1128@gmail.com>
2023-01-12 19:45:38 +0900

    User Schema 작성 / GraphQL API tets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo'
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { join } from 'path';
 
 console.log(process.env.NODE_ENV);
 
@@ -12,9 +15,13 @@ console.log(process.env.NODE_ENV);
     ConfigModule.forRoot({
       envFilePath: ['.env.', process.env.NODE_ENV].join("")
     }),
-    MongooseModule.forRoot(process.env.MONGO_DB_URL)
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }),
+    UserModule
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
