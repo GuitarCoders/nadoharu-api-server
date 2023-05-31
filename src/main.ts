deec7e476c19b5232094c90611c3353836b5812e
JongCo <xn1128@gmail.com>
2023-05-31 17:34:21 +0900

    cors 제거

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,6 @@ install();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "*, *",
-    methods: [
-      "GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"
-    ]
-  });
   await app.listen(6378);
 }
 bootstrap();
