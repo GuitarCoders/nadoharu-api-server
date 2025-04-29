ad573b994c648e80a00ad48410b0346b1228e2aa
JongCo <xn1128@gmail.com>
2025-04-29 16:46:28 +0900

    여러 user들을 쿼리해 friend여부를 붙혀 반환하는 함수 작성

--- src/userInfo/userInfo.service.ts
+++ src/userInfo/userInfo.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { FriendService } from "src/friend/friend.service";
 import { UserService } from "src/user/user.service";
-import { UserInfoDto } from "./dto/userInfo.dto";
+import { UserInfoDto, UserInfosDto } from "./dto/userInfo.dto";
 import { FriendState } from "./enums/userInfo.enum";
 
 @Injectable()
@@ -11,30 +11,63 @@ export class UserInfoService {
         private FriendService: FriendService
     ) {}
 
+    async getUserInfos(
+        requestUserId: string,
+        search: string
+    ): Promise<UserInfosDto> {
+        try {
+            const users = await this.UserService.findUsers(search)
+            const userInfoPromises: Promise<UserInfoDto>[] = users.Users.map(async(user) => {
+                const isFriend = await this.getFriendState(requestUserId, user._id);
+                return {
+                    user,
+                    isFriend
+                }
+            });
+            const userInfos: UserInfoDto[] = await Promise.all(userInfoPromises);
+
+            return {
+                users: userInfos
+            }
+            
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     async getUserInfo(
         requestUserId: string, 
         targetUserId: string
     ): Promise<UserInfoDto>{
         try {
             const targetUser = await this.UserService.getUserByIdSafe(targetUserId);
-            const isFriend = await this.FriendService.checkFriend(requestUserId, targetUserId);
-
-            let friendState:FriendState
-
-            if (requestUserId === targetUserId) {
-                friendState = FriendState.ME;
-            } else {
-                if (isFriend) friendState = FriendState.FRIEND;
-                else friendState = FriendState.STRANGER;
-            }
+            const isFriend = await this.getFriendState(requestUserId, targetUserId);
             
             return {
                 user: targetUser,
-                isFriend: friendState
+                isFriend
             }
             
         } catch (err) {
             console.error(err);
+        }
+    }
+
+    async getFriendState(userId1: string, userId2: string): Promise<FriendState> {
+        try {
+            const isFriend = await this.FriendService.checkFriend(userId1, userId2);
+
+            if (userId1 === userId2) {
+                return FriendState.ME;
+            } else {
+                if (isFriend) return FriendState.FRIEND;
+                else return FriendState.STRANGER;
+            }
+
+            return FriendState.STRANGER;
+
+        } catch (e) {
+            console.error(e);
         }
     }
 }
