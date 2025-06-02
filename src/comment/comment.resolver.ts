381491b6c67818fddd647f568c28e711ca126587
jongco <xn1128@gmail.com>
2025-06-03 03:08:57 +0900

    NDHR-75 comment 관련 graphQL description 작성

--- src/comment/comment.resolver.ts
+++ src/comment/comment.resolver.ts
@@ -14,21 +14,31 @@ export class CommentResolver {
         private readonly CommentService: CommentService
     ) {}
 
-    @Query(() => CommentsDto, {name: "comments"})
+    @Query(
+        () => CommentsDto, 
+        {
+            name: "comments",
+            description: "특정 글의 댓글을 가져오는 쿼리입니다."
+        })
     @UseGuards(GqlAuthGuard)
     async getCommentByPostId (
-        @Args('postId') targetPostId: string,
-        @Args('pagination') pagination: PaginationInput
+        @Args('postId', {description: "댓글을 가져올 대상 글의 id"}) targetPostId: string,
+        @Args('pagination', {description: "페이지네이션 정보"}) pagination: PaginationInput
     ): Promise<CommentsDto> {
         return await this.CommentService.getCommentsByPostId(targetPostId, pagination);
     }
 
-    @Mutation(() => CommentDto, {name: "addCommentToPost"})
+    @Mutation(
+        () => CommentDto, 
+        {
+            name: "addCommentToPost",
+            description: "특정 글에 댓글을 작성하는 뮤테이션입니다."
+        })
     @UseGuards(GqlAuthGuard)
     async addCommentToPost(
         @CurrentUser() user: UserJwtPayload,
-        @Args('targetPostId') targetPostId: string,
-        @Args('content') content: string,
+        @Args('targetPostId', {description: "댓글을 작성할 대상 글의 id"}) targetPostId: string,
+        @Args('content', {description: "댓글의 내용"}) content: string,
     ): Promise<CommentDto> {
         return await this.CommentService.addCommentToPost(
             user._id, 
@@ -43,7 +53,7 @@ export class CommentResolver {
     @UseGuards(GqlAuthGuard)
     async deleteCommentById(
         @CurrentUser() user: UserJwtPayload,
-        @Args('targetCommentId') targetCommentId: string
+        @Args('targetCommentId', {description: "삭제할 댓글의 id"}) targetCommentId: string
     ): Promise<deleteCommentResultDto> {
         return await this.CommentService.deleteCommentById(targetCommentId, user._id);
     }
