44520e0da92cc886750d9051e3864e510f2c279b
jongco <xn1128@gmail.com>
2025-06-20 15:52:00 +0900

    .env에서 port를 설정할 수 있도록 변경

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { install } from 'source-map-support'
 install();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(6378);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 6378);
+
+  await app.listen(port);
 }
 bootstrap();
