598e9d7794583fa065f4d2098f7d0558706958c1
jongco <xn1128@gmail.com>
2025-07-03 20:48:35 +0900

    NDHR-178 postsByMe에 나도공유 글을 함께 응답하는 기능 작성

--- src/post/post-aggregator/post-aggregator.resolver.ts
+++ src/post/post-aggregator/post-aggregator.resolver.ts
@@ -14,6 +14,23 @@ export class PostAggregatorResolver {
     ) {}
 
     @Query(() => PostsQueryResultDto, { 
+        name: "postsByMe",
+        description: "로그인한 유저가 작성한 글 목록을 가져오는 쿼리입니다."
+    })
+    @UseGuards(GqlAuthGuard)
+    async getPostsByMe(
+        @CurrentUser() user: UserJwtPayload,
+        @Args('filter', {
+            description: "작성한 글의 카테고리 등의 분류를 위한 필터. 아직 카테고리등의 기능을 구현하지 않았으므로 필터 안의 내용을 비워서 쿼리를 보내주시기 바랍니다."
+        }) filter: PostFilterInput,
+        @Args('pagination', {
+            description: "페이지네이션 정보"
+        }) pagination: PaginationInput
+    ): Promise<PostsQueryResultDto> {
+        return await this.PostAggregatorService.getPostsByUserId(user._id, filter, pagination);
+    }
+
+    @Query(() => PostsQueryResultDto, { 
         name: "postsByUserId",
         description: "특정 유저의 id를 통해 해당 유저가 작성한 글 목록을 가져오는 쿼리입니다."
     })
--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -27,57 +27,6 @@ export class PostResolver {
     ): Promise<PostDto> {
         return await this.PostService.getPostById(targetPostId);
     }
-
-    // @Query(() => PostsQueryResultDto, { 
-    //     name: "postsByUserId",
-    //     description: "특정 유저의 id를 통해 해당 유저가 작성한 글 목록을 가져오는 쿼리입니다."
-    // })
-    // @UseGuards(GqlAuthGuard)
-    // async getPostsByUserId(
-    //     @CurrentUser() user: UserJwtPayload,
-    //     @Args('targetUserId', {
-    //         description: "글 작성자의 id"
-    //     }) targetUserId: string,
-    //     @Args('filter', {
-    //         description: "작성한 글의 카테고리 등의 분류를 위한 필터. 아직 카테고리등의 기능을 구현하지 않았으므로 필터 안의 내용을 비워서 쿼리를 보내주시기 바랍니다."
-    //     }) filter: PostFilterInput,
-    //     @Args('pagination', {
-    //         description: "페이지네이션 정보"
-    //     }) pagination: PaginationInput
-    // ): Promise<PostsQueryResultDto> {
-    //     return await this.PostService.getPostsByUserId(targetUserId, filter, pagination);
-    // }
-
-    // @Query(() => PostsQueryResultDto, { 
-    //     name: "postsForTimeline",
-    //     description: "로그인한 유저 기준 타임라인에 표시될 글 목록을 가져오는 쿼리입니다. 로그인한 유저를 포함하여 친구들의 글들을 시간순으로 가져옵니다."
-    // })
-    // @UseGuards(GqlAuthGuard)
-    // async getPostsForTimeline(
-    //     @CurrentUser() user: UserJwtPayload,
-    //     @Args('pagination', {
-    //         description: "페이지네이션 정보"
-    //     }) pagination: PaginationInput
-    // ): Promise<PostsQueryResultDto> {
-    //     return await this.PostService.getPostsForTimeline(user._id, pagination);
-    // }
-
-    @Query(() => PostsQueryResultDto, { 
-        name: "postsByMe",
-        description: "로그인한 유저가 작성한 글 목록을 가져오는 쿼리입니다."
-    })
-    @UseGuards(GqlAuthGuard)
-    async getPostsByMe(
-        @CurrentUser() user: UserJwtPayload,
-        @Args('filter', {
-            description: "작성한 글의 카테고리 등의 분류를 위한 필터. 아직 카테고리등의 기능을 구현하지 않았으므로 필터 안의 내용을 비워서 쿼리를 보내주시기 바랍니다."
-        }) filter: PostFilterInput,
-        @Args('pagination', {
-            description: "페이지네이션 정보"
-        }) pagination: PaginationInput
-    ): Promise<PostsQueryResultDto> {
-        return await this.PostService.getPostsByUserId(user._id, filter, pagination);
-    }
         
     @Mutation(() => CreatePostResultDto, { 
         name: "createPost",
